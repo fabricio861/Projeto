d35396e3bba2a87f33a930605adf278290a14e9c
fabricio861 <fabriciomansano340@gmail.com>
2025-11-09 23:46:12 -0300

    9/11

--- minimal-api/Infraestrutura/Db/DbContexto.cs
+++ minimal-api/Infraestrutura/Db/DbContexto.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Entidades;
 
 namespace minimal_api.Infraestrutura.Db
 {
-    public class DbContexto
+    public class DbContexto : DbContext
     {
-        
+        public DbSet<Administrador> Administradores { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("string de conexão", ServerVersion.AutoDetect("string de conexão"));
+        }
     }
 }
